d7abbc9087efe2e85d1568913c6d4b09b55fc64e
artem <artemasarkisan598@gmail.com>
2025-10-15 17:51:37 +0300

    py-burger-recipe

--- app/main.py
+++ app/main.py
@@ -1,14 +1,65 @@
-class Validator:
-    pass
+from abc import ABC, abstractmethod
+from typing import Any
+
+
+class Validator(ABC):
+
+    def __set_name__(self, owner: Any, name: Any) -> Any:
+        self._protected_name = "_" + name
+
+    def __get__(self, instance: Any, owner: Any) -> Any:
+        return getattr(instance, self._protected_name)
+
+    def __set__(self, instance: Any, value: Any) -> Any:
+        self.value = value
+        setattr(instance, self._protected_name, value)
+
+    @abstractmethod
+    def validate(self) -> None:
+        pass
 
 
 class Number:
-    pass
+
+    def __init__(self, min_value: int, max_value: int) -> None:
+        self.min_value = min_value
+        self.max_value = max_value
+
+    def validate(self, value: int) -> Any:
+        if not isinstance(value, int):
+            raise TypeError("Quantity should be integer.")
+        if not (self.min_value <= value <= self.max_value):
+            raise ValueError(
+                f"Quantity should not be less than {self.min_value}"
+                f" and greater than {self.max_value}."
+            )
 
 
 class OneOf:
-    pass
+
+    def __init__(self, options: str) -> None:
+        self.options = options
+
+    def validate(self, value: Any) -> Any:
+        if value not in self.options:
+            raise ValueError(
+                f"Expected {value} to be one of {self.options}."
+            )
 
 
 class BurgerRecipe:
-    pass
+    buns = Number(2, 3)
+    cheese = Number(0, 2)
+    tomatoes = Number(0, 3)
+    cutlets = Number(1, 3)
+    eggs = Number(0, 2)
+    sauce = OneOf("ketchup", "mayo", "burger")
+
+    def __init__(self, buns: int, cheese: int,
+                 tomatoes: int, cutlets: int, eggs: int, sauce: str) -> None:
+        self.buns = buns
+        self.cheese = cheese
+        self.tomatoes = tomatoes
+        self.cutlets = cutlets
+        self.eggs = eggs
+        self.sauce = sauce
